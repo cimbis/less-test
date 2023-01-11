d0ead33a768a6b00744641fe54a9b4fc4d49ea7e
jc <cimbulis.j@gmail.com>
2023-01-11 13:53:57 +0200

    test v16 with less

---
+++ src/App.tsx
@@ -0,0 +1,88 @@
+import "@ids-core/button"
+import "@ids-core/typography";
+import "@ids-core/color";
+import "@ids-core/panel/src/panel.less";
+import "@ids-core/checkbox/src/checkbox.less";
+import "@ids-core/radio-buttons/src/radio-buttons.less";
+
+function App() {
+    return (
+        <div className="if app">
+            <div className="if container">
+                <div className="if panel is-expandable light is-open">
+                    <div className="if title" aria-controls="exp1" aria-expanded="true">
+                        Tips och råd
+                    </div>
+                    <div className="if content" role="region" id="exp1">
+                        <button className="if button primary" type="button">A button</button>
+                    </div>
+                </div>
+            </div>
+
+            <div className="if container">
+                <form autoComplete="off" action="">
+                    <div className="if input-wrapper">
+                        <div className="if radio-buttons">
+                            <input
+                                type="radio"
+                                id="radio-buttons-section-md-radio-button-1"
+                                className="if radio-button"
+                                name="radio-buttons"
+                            />
+                            <label htmlFor="radio-buttons-section-md-radio-button-1">
+                                Default
+                            </label>
+
+                            <input type="radio"
+                                   id="radio-buttons-section-md-radio-button-2"
+                                   className="if radio-button is-hovered"
+                                   name="radio-buttons"
+                            />
+                            <label htmlFor="radio-buttons-section-md-radio-button-2">
+                                Hovered
+                            </label>
+
+                            <input type="radio"
+                                   id="radio-buttons-section-md-radio-button-3"
+                                   className="if radio-button is-active"
+                                   name="radio-buttons"
+                            />
+                            <label htmlFor="radio-buttons-section-md-radio-button-3">
+                                Pressed/Active
+                            </label>
+
+                            <input
+                                type="radio"
+                                id="radio-buttons-section-md-radio-button-4"
+                                className="if radio-button is-focused"
+                                name="radio-buttons"
+                            />
+                            <label htmlFor="radio-buttons-section-md-radio-button-4">
+                                Focused
+                            </label>
+
+                            <input type="radio" id="radio-buttons-section-md-radio-button-5"
+                                   className="if radio-button is-invalid" name="radio-buttons"
+                            />
+                            <label htmlFor="radio-buttons-section-md-radio-button-5">
+                                Error
+                            </label>
+
+                            <input type="radio"
+                                   disabled={ true }
+                                   id="radio-buttons-section-md-radio-button-6"
+                                   className="if radio-button"
+                                   name="radio-buttons"
+                            />
+                            <label htmlFor="radio-buttons-section-md-radio-button-6">
+                                Disabled
+                            </label>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default App
